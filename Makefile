613bb22d2418e499a80753db485040a3c1e5614a
Rhydon <adirmak1@gmail.com>
2020-10-02 13:34:34 +0300

    improved environment

--- Makefile
+++ Makefile
@@ -1,22 +1,21 @@
-MODULENAME := <INSERT_MODULE_NAME>
+MODULENAME := kernel_development
 
 obj-m += $(MODULENAME).o
 
-$(MODULENAME)-y += src/<FILES>
+$(MODULENAME)-y += main.o
 
-ccflags-y := -O0 -Wno-declaration-after-statement -Wno-discarded-qualifiers
+KERNELDIR ?= ~/workspace/buildroot-2020.02.4/output/build/linux-4.19.91
 
-KERNELDIR ?= ~/workspace/buildroot/output/build/linux-4.19.98
-# KERNELDIR ?= /lib/modules/4.10.0-38-generic/build
 PWD       := $(shell pwd)
 
-debug:
-	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+all: debug
+
 release:
 	$(MAKE) -C $(rm -rf *.o *~ core .depend .*.cmd *.ko *.mod.c .tmp_versions debug) M=$(PWD) modules
-install:
-	sudo insmod $(MODULENAME).ko
-remove:
-	sudo rmmod $(MODULENAME).ko
+
+ccflags-y := -g -Og -O0
+debug:
+	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+
 clean:
 	rm -rf *.o *~ core .depend .*.cmd *.ko *.mod.c .tmp_versions debug
--- debug.sh
+++ debug.sh
@@ -2,41 +2,38 @@
 trap "" 0 1 2 3 9 13 15
 
 # User configuration
-HOME_DIRECTORY="/home/spiderpig"
-BUILDROOT_IMAGES_PATH="$HOME_DIRECTORY/workspace/buildroot/output/images"
+HOME_DIRECTORY="/home/rhydon"
+BUILDROOT_IMAGES_PATH="$HOME_DIRECTORY/workspace/buildroot-2020.02.4/output/images"
 VM_USERNAME="root"
-VM_PASSWORD="root"
-KERNEL_MODULE_NAME="kfile_over_icmp"
+VM_PASSWORD="123456"
+KERNEL_MODULE_NAME="kernel_development"
 
 CWD=`pwd`
 REMOTE_DIR="/root/"`basename $CWD`
 SSH_PORT=5555
+SNAPSHOT_NAME="first_snapshot"
 
 cd $BUILDROOT_IMAGES_PATH
 
 if pgrep "qemu" > /dev/null
 then
-    echo "[+] QEMU is already running, continuing"
-else
-    echo "[+] running QEMU emulation"
-    setsid qemu-system-x86_64 -enable-kvm -cpu host -s -kernel bzImage -m 2048 -hda rootfs.qcow2 -append "root=/dev/sda rw nokaslr" -smp 4 -net nic -net user,hostfwd=tcp::$SSH_PORT-:22,hostfwd=tcp::3333-:33 &
+    pkill -9 qemu
 fi
+
+echo "[+] running QEMU emulation"
+setsid qemu-system-x86_64 -enable-kvm -cpu host -s -kernel bzImage -m 4096 -hda rootfs.qcow2 -append "root=/dev/sda rw nokaslr" -smp 4 -net nic,model=virtio -net user,hostfwd=tcp::$SSH_PORT-:22 -loadvm $SNAPSHOT_NAME &
 
 # Busy loop for waiting for the vm to startup and setup ssh
 until sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT -o StrictHostKeyChecking=no -q $VM_USERNAME@localhost exit
 do 
     echo "[+] waiting for the VM to initialize"
-    sleep 2
+    sleep 1
 done
 
 echo "[+] VM: initialized"
 
 echo "[+] moving kernel module $KERNEL_MODULE_NAME to the vm"
-sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost "rm -rf $REMOTE_DIR"
 sshpass -p "$VM_PASSWORD" scp -P $SSH_PORT -r $CWD $VM_USERNAME@localhost:$REMOTE_DIR
-
-echo "[+] unloading $KERNEL_MODULE_NAME from the kernel (if exists)"
-sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost "rmmod $REMOTE_DIR/$KERNEL_MODULE_NAME.ko"
 
 echo "[+] loading $KERNEL_MODULE_NAME to the kernel"
 sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost "insmod $REMOTE_DIR/$KERNEL_MODULE_NAME.ko"
@@ -59,10 +56,5 @@ else
     touch $GDBINIT_PATH
 fi
 
-echo "set auto-load safe-path /" >> $GDBINIT_PATH
 echo "add-symbol-file $CWD/$KERNEL_MODULE_NAME.ko $text_address" >> $GDBINIT_PATH
 echo "file $CWD/$KERNEL_MODULE_NAME.ko" >> $GDBINIT_PATH
-
-echo "[+] launching SSH connection to the VM"
-pkill gnome-terminal
-/usr/bin/dbus-launch /usr/bin/gnome-terminal --working-directory=$CWD -e "sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost"
